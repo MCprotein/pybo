d28513a993a231f5a2d73ab73d3dae8dee43fad2
MCprotein <chss3339@gmail.com>
2022-02-04 00:58:27 +0900

    404 페이지 변경

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
-# from pybo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +10,5 @@ urlpatterns = [
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'), # '/'에 해당되는 path
 ]
+
+handler404 = 'common.views.page_not_found'
