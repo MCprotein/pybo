81ecc3435454c4834c12d269e126a0243c1fd67d
MCprotein <chss3339@gmail.com>
2022-02-03 18:25:10 +0900

    settings 변경

---
+++ config/settings/local.py
@@ -0,0 +1,4 @@
+from .base import *
+
+ALLOWED_HOSTS = []
+# base.py 파일의 모든 내용을 사용하지만 ALLOWED_HOSTS만 다르게 설정
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+ALLOWED_HOSTS = ['3.39.5.116']
+# 보통 운영 환경을 production 환경이라고 한다.
